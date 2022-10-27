f9031596fae76ee6e6057aeefd79f7427b5f908f
lmariap <116771875+lmariap@users.noreply.github.com>
2022-10-26 21:31:36 -0300

    Add files via upload

---
+++ Pega soma/pega_soma.py
@@ -0,0 +1,16 @@
+""" 
+Programa: Pega_numero
+Requisito: Este programa pega um numero digitado pelo usuário e o armazena em memória
+Autor: Letícia Maria Pereira
+Data: 24/10/2022
+Versão: 0.0.1
+"""
+
+# Entrada de dados
+# A função para ler os dados do teclado é input ()
+numero1 = 7 # definição de variável
+numero2 = input("Digite um numero: ")
+
+# Processamento de dados
+# Saída de dados
+Print ("Hello, world!")
---
+++ aula 1/hello.py
@@ -0,0 +1,11 @@
+#Para escrever comentários em Python, use o hashtag
+""" 
+se quiser fazer comentárias de múltiplas linhas, 
+use três aspas duplas
+"""
+
+# Programação Sequencial
+# Entrada de dados
+# Processamento de dados
+# Saída de dados
+Print ("Hello, world!")
